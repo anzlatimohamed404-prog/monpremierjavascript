3fa24fcd952eb53d383f8ba02ae88d355ed848d9
anzlatimohamed404-prog <anzlatimohamed404@gmail.com>
2025-12-04 16:05:13 +0300

    création gestion de bibliothéque

---
+++ gestionBibliotheque.js
@@ -0,0 +1,9 @@
+let totalLivres = 500;
+totalLivres+= 50
+totalLivres-= 10
+totalLivres+= 5
+console.log(totalLivres);
+
+let affichageTotalLivres = "le nombre total de livre est de ";
+affichageTotalLivres+= totalLivres
+console.log(affichageTotalLivres);
---
+++ script.js
@@ -0,0 +1,69 @@
+// ici, je rédige mon script javascript
+
+let nom ="Said Mohamed";
+        let prenom = "andjouzati";
+        console.log("Bonjour je m'appelle",nom,prenom);
+        let age = 18;
+        console.log("j'ai",age);
+        let prix = 12;
+        const nombreDecimal = 3.4123;
+        const nombreNegatif = -509;
+        const nombreFraction = 1/3;
+        console.log("prix: ",prix);
+        console.log("nombre décimal: ", nombreDecimal);
+        console.log("nombre négatif: ", nombreNegatif);
+        console.log("nombre fraction: ", nombreFraction);
+
+        let salutation = "bonjour les gens";
+         console.log(salutation);
+
+        let greenting = 'good morning';
+        console.log(greenting);
+
+        let andabou ='kwezi';
+        console.log(andabou);
+
+        let commune = "m'tsamboro";
+        console.log(commune);
+
+         
+        let village = "m\'tsahara";
+        console.log(village);
+
+        let vrai = true;
+        console.log(vrai);
+
+        let est_faux = false;
+        console.log(est_faux);
+
+        
+        // je déclare un tableau.
+        let eleves = ["said", "amina", "bao","anzlati","anrabia","irwane","sabrina","habachia","rafou","andjouzati","linda"];
+        console.log(eleves);
+        console.log(eleves[0]);// récuperation du 1er membre du tableau grace au chiffre zero.
+        console.log(eleves[1]);// récuperation du 2éme membre du tableau grace au chiffre.
+        console.log(eleves[2]);// récuperation du 3éme membre du tableau grace au chiffre.
+        console.log(eleves[3]);// récuperation du 4éme membre du tableau grace au chiffre.
+        console.log(eleves[4]);// récuperation du 5éme membre du tableau grace au chiffre.
+        console.log(eleves[5]);// récuperation du 6éme membre du tableau grace au chiffre.
+        console.log(eleves[6]);// récuperation du 7éme membre du tableau grace au chiffre.
+        console.log(eleves[7]);// récuperation du 8éme membre du tableau grace au chiffre.
+        console.log(eleves[8]);// récuperation du 9éme membre du tableau grace au chiffre.
+        console.log(eleves[9]);// récuperation du 10éme membre du tableau grace au chiffre.
+        console.log(eleves[10]);// récuperation du 11éme membre du tableau grace au chiffre .
+        // récuperation de chaque membres.
+
+
+        let diplome = [true,10, "daniel"];
+        console.log(diplome);
+
+        let client = {
+            nom: "bacar",
+            village: "choungui",
+            age: 25,
+            scores: [4,7,2]
+        };
+
+        console.log(client);
+
+       
