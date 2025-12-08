55f7ca891a75ab122185167aa3b08f49d2539a1f
anzlatimohamed404-prog <anzlatimohamed404@gmail.com>
2025-12-08 16:33:44 +0300

    addition des nombres

---
+++ operateur.js
@@ -0,0 +1,13 @@
+//ici je code les operateurs en javascript.
+
+let prixUnitaire = 3; 
+
+let valeurAjoutee = 7;
+// premiere technique d'addition des nombres
+let prixAdditionne1 = 3+7;
+// j'affiche la valeur de la variable prixAdditione1
+console.log("prixAdditionne1 :", prixAdditionne1 );
+
+// deuxiéme méthode d'additionner des nombres à l'aide de variables
+let prixAdditione2 = prixUnitaire + valeurAjoutee;
+console.log("prixAdditionne2 : ", prixAdditione2);
--- script.js
+++ script.js
@@ -62,8 +62,47 @@ let nom ="Said Mohamed";
             village: "choungui",
             age: 25,
             scores: [4,7,2]
+
         };
 
         console.log(client);
+
+        // je déclare mon objet "joueurprofessonnel".
+        let joueurprofessionnel = {
+             nom:"zindine",// la propriéter "nom"désigne le nom du joueur
+            prenom:"yazide zidane",//la propriéter "prenom"désigne le prenom du joueur
+            nationalite: ["Française" ," Algerienne"],// la propriéter "nationnalité"désigne la nationnalité du joueur
+            datedenaissance:"23/06/1972",//la propriéter "datedenaissance"désigne la date de naissance du joueur
+            taille:"1,85",//la propriéter "taille"désigne la taille du joueur
+            piedfort:"droit",//la propriéter "piedfort"désigne le pied fort du joueur
+            poste:"milieu offensif"//la propriéter "poste"désigne le post du joueur
+
+        };
+        // je déclare mon objet "députéefrançaise".
+        // je défini un objet qui represente une journaliste du nom de "youssouffa estelle".
+        let deputeefrancaise = {
+            nom:"Youssouffa",//la propriéter "nom"désigne le nom de la journaliste
+            prenom:"Estelle",//la propriéter "prenom"désigne le prenom de la journaliste
+            datedenaissance:"31 juillet 1978",///la propriéter "datedenaissance"désigne la date de naissance de la journaliste
+            lieudenaissance:"Châtenay-Malabry (France)",///la propriéter "lieudenaissance"désigne le lieudenaissancede la journaliste
+            nationalite:"Française",//la propriéter "nationnalité"désigne la nationnalié de la journaliste
+            election:"19 juin 2022",//la propriéter"election" désigne l'election de la journaliste
+            reelection:"30 juin 2024",//la propriéter "reelection"désigne la reelection de la journaliste
+            prefession:"Journaliste",//la propriéter "profession"désigne la profession de la journaliste
+            partipolitique:"UDI (depuis 2022)",///la propriéter "partipolitique"désigne la partipolitique de la journaliste
+            diploméede:	"IUT de Tours",//la propriéter "diplomé"désigne le diplome de la journaliste
+            Prédécesseur:"Ramlati Al",//la propriéter "precesseur"désigne le precesseur de la journaliste
+            groupepolitique:"Ramlati Al",//la propriéter "groupepolitique"désigne le groupe politique de la journaliste
+            Circonscription:"1re de Mayotte"//la propriéter "circonscription"désigne la circonscription de la journaliste
+        }
+
+        console.log(deputeefrancaise);
+        
+            
+        
+             
+        
+
+       
 
        
