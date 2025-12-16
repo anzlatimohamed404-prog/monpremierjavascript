c1a245ca1bd615ca737d5e4e0c85772bfb73a93c
anzlatimohamed404-prog <anzlatimohamed404@gmail.com>
2025-12-16 08:28:05 +0300

    création de formulaire

--- dom_calcul_age.js
+++ dom_calcul_age.js
@@ -1,5 +1,10 @@
 function afficherAge() {
 
+    // je récupére un élement (une balise) situé dans le fichier HTML (calcul_age.html)
+    // j'utlise l'identifiant pour trouver le bon élément. l'élément récupérer est ensuite
+    //stocker dans la variable elementBoutonValider.
+       
+
     let nom = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
     let situation = document.getElementById("situation").value;
@@ -17,3 +22,25 @@ function afficherAge() {
     document.getElementById("resultat").textContent =
         `${prenom} ${nom}, vous êtes ${situation} et vous avez ${age} ans.`;
 }
+
+let elementBoutonValider = document.getElementById("boutonValider");
+elementBoutonValider.addEventListener("click", function(){
+    console.log("elementBoutonValider :", elementBoutonValider);
+});
+//console.log(document.getElementBYID("nom"));
+console.log(document.getElementById("nom"));
+
+console.log(document.getElementById)
+
+console.log(document.title);
+
+console.log(document.head);
+
+//je récupére l'élément html (balise) dont l,identifiant est recensement.
+let elementDivRecensement = document.querySelector("#recensement");
+
+console.log(elementDivRecensement);
+
+let elementPRecensement = document.querySelector("#paragraphe1");
+
+console.log(elementPRecensement);
